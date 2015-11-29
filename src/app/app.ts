da7c7640df8f17f751dc3e65051ed9a3d3af17c5
Przemek <przemyslaw.kalka@gmail.com>
2015-11-29 11:50:54 +0000

    Structure changes - moving contents to separate files

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,48 +1,10 @@
 import {Component, bootstrap, FORM_DIRECTIVES, CORE_DIRECTIVES} from 'angular2/angular2';
-
-class Hero {
-    id: number;
-    name: string;
-}
+import { Hero } from './hero';
 
 @Component({
     selector: 'my-app',
-    template:`
-      <h1>{{title}}</h1>
-      <h2>My Heroes</h2>
-      <ul class="heroes">
-        <li *ng-for="#hero of heroes"
-          [ng-class]="getSelectedClass(hero)"
-          (click)="onSelect(hero)">
-          <span class="badge">{{hero.id}}</span> {{hero.name}}</a>
-        </li>
-      </ul>
-      <div *ng-if="selectedHero">
-        <h2>{{selectedHero.name}} details!</h2>
-        <div><label>id: </label>{{selectedHero.id}}</div>
-        <div>
-          <label>name: </label>
-          <input [(ng-model)]="selectedHero.name" placeholder="name"></input>
-        </div>
-      </div>
-    `,
-    directives: [FORM_DIRECTIVES, CORE_DIRECTIVES],
-    styles:[`
-      .heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}
-      .heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }
-      .heroes li:hover {color: #369; background-color: #EEE; left: .2em;}
-      .heroes .badge {
-        font-size: small;
-        color: white;
-        padding: 0.1em 0.7em;
-        background-color: #369;
-        line-height: 1em;
-        position: relative;
-        left: -1px;
-        top: -1px;
-      }
-      .selected { background-color: #EEE; color: #369; }
-    `],
+    templateUrl: 'app/app.html',
+    directives: [FORM_DIRECTIVES, CORE_DIRECTIVES]
 })
 
 class AppComponent {
